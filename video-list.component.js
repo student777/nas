52dc470497224b1504f46fb217626d4a3ca18eab
student777 <darkdakku@gmail.com>
2018-07-16 16:17:02 +0900

    Update index template

--- video-list.component.js
+++ video-list.component.js
@@ -2,7 +2,7 @@ angular.
 	module('sagetime').
 	component('videoList', {
 	template:'<div class="list-group">'+
-				'<a ng-repeat="video in $ctrl.videos" href="{{video.src}}" class="list-group-item list-group-item-action">{{video.name}}</a>'+
+				'<a ng-repeat="video in $ctrl.videos" href="video/{{video.name}}/{{video.filename}}" class="list-group-item list-group-item-action">{{video.name + "/" + video.sauce}}</a>'+
 			'</div>',
 	controller: function VideoListController($http) {
 		var self = this;

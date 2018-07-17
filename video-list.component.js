fab1a12d029a1da68f0beff4b561ffcfd193ce67
student777 <darkdakku@gmail.com>
2018-07-17 12:46:17 +0900

    Add modal & Support mkv, avi

--- video-list.component.js
+++ video-list.component.js
@@ -6,6 +6,14 @@ angular.
 		var self = this;
 		$http.get('data.json').then(function(response){
 			self.videos = response.data;
-		})
+		});
+		this.play_video = function(video){
+			$('#modal-title').text(video.sauce)
+			var modal_video = $('#modal_video').get(0)
+			modal_video.pause();
+			$('#modal_source').attr('src', '/video/'+ video.name + '/' + video.filename)
+			modal_video.load();
+			modal_video.play();
+		};
 	}
 });

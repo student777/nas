9a1f24db5bcd3c2231b75badfffb1a3376a74dac
student777 <darkdakku@gmail.com>
2018-07-28 11:16:17 +0900

    Update ftp json py

--- scripts/make_videolink.py
+++ scripts/make_videolink.py
@@ -1,13 +1,29 @@
 import json
-from ftplib import FTP
-ftp = FTP('asdf.xyz', user="asdf", passwd="zxcv")
+from ftplib import FTP, error_temp
+
+host = input('host: ')
+user = input('user: ')
+pwd = input('passwd: ')
+path = input('path: ')
+
+ftp = FTP(host, user=user, passwd=pwd)
 # ftp.login()
-ftp.cwd('HDD1/zxcv')
+wd = '/HDD1/{}'.format(path)
+ftp.cwd(wd)
 name_list = ftp.nlst()
 f = open('output', 'w')
+f.write('[\n')
 for name in name_list:
-    src_list = ftp.nlst('/HDD1/zxcf/' + name + '/')
-    for src in src_list:
-        f.write(json.dumps({"sauce": src.split('/')[-1].lower(), "filename": src.split('/')[-1], "name": name}) + ',\n')
+    if name == 'Away' or name == 'dfdfdf':
+        continue
+    try:
+        src_list = ftp.nlst(wd + '/' + name + '/')
+    except error_temp:
+        continue
 
+    for src in src_list:
+        if src.endswith('mp4') or src.endswith('avi') or src.endswith('mkv'):
+            f.write(json.dumps({"sauce": src.split('/')[-1].lower(), "filename": src.split('/')[-1], "name": name}) + ',\n')
+
+f.write(']')
 f.close()
